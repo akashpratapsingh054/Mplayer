0acc4aa6be55eddd3ddba49d6f24b60a42b094ce
akashpratapsingh054 <akashpratapsingh.054@gmail.com>
2017-07-17 23:33:06 +0530

    Music Player
    Contributor-Akash PratapSingh(github.com/akashpratapsingh054).....Mayank Dixit(github.com/mayankdixit417

---
+++ app/src/main/java/com/example/akash/mplayer/SongsManager.java
@@ -0,0 +1,63 @@
+package com.example.akash.mplayer;
+
+import android.database.Cursor;
+import android.provider.MediaStore;
+import android.content.Context;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SongsManager {
+    // SDCard Path
+    final String MEDIA_PATH = new String("/sdcard/");
+    private ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+
+    // Constructor
+    public SongsManager(){
+
+    }
+
+
+
+    /**
+     * Function to read all mp3 files from sdcard
+     * and store the details in ArrayList
+     * */
+    public ArrayList<HashMap<String, String>> getAudioList(Context context) {
+
+        ArrayList<HashMap<String, String>> mSongsList = new ArrayList<HashMap<String, String>>();
+        Cursor mCursor = context.getContentResolver().query(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                new String[] { MediaStore.Audio.Media.DISPLAY_NAME,
+                        MediaStore.Audio.Media.DATA }, null, null, null);
+
+        int count = mCursor.getCount();
+        System.out.println("total no of songs are=" + count);
+        HashMap<String, String> songMap;
+        while (mCursor.moveToNext()) {
+            songMap = new HashMap<String, String>();
+            songMap.put(
+                    "songTitle",
+                    mCursor.getString(mCursor
+                            .getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)));
+            songMap.put("songPath", mCursor.getString(mCursor
+                    .getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)));
+            mSongsList.add(songMap);
+        }
+        mCursor.close();
+        return mSongsList;
+    }
+
+
+
+    /**
+     * Class to filter files which are having .mp3 extension
+     * */
+    class FileExtensionFilter implements FilenameFilter {
+        public boolean accept(File dir, String name) {
+            return (name.endsWith(".mp3") || name.endsWith(".MP3"));
+        }
+    }
+}
